5d798a15f121e9c46a306139dcb2b0cea0158703
Franco Pertusati <pertusatifranco836@gmail.com>
2025-05-08 20:40:47 -0300

    Bento projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NavbarComponent } from "./sections/navbar/navbar.component";
 import { StackComponent } from "./sections/stack/stack.component";
 import { ProjectsComponent } from "./sections/projects/projects.component";
 import { AboutMeComponent } from "./sections/about-me/about-me.component";
+import { BentoProjectsComponent } from "./sections/bento-projects/bento-projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, AboutMeComponent, ProjectsComponent, StackComponent, PresentationCardComponent],
+  imports: [NavbarComponent, AboutMeComponent, StackComponent, PresentationCardComponent, BentoProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/sections/bento-projects/bento-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bento-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './bento-projects.component.html',
+  styleUrl: './bento-projects.component.css'
+})
+export class BentoProjectsComponent {
+
+}
