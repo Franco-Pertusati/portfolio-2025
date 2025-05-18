89498c8dcfc1c33e34e737dca0ddc62fd4dde404
Franco Pertusati <pertusatifranco836@gmail.com>
2025-05-18 09:23:33 -0300

    Lista de tecnologias con marquee

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../shared/nav-bar/nav-bar.component";
 import { ThemeBtnComponent } from "../shared/theme-btn/theme-btn.component";
 import { PresentationCardComponent } from "./presentation-card/presentation-card.component";
+import { StackComponent } from "./stack/stack.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, PresentationCardComponent],
+  imports: [NavBarComponent, PresentationCardComponent, StackComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
