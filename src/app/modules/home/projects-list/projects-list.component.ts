a3d6cf10e17fcecab99028b66bc89f2cc978de78
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-10 10:48:28 -0300

    Descripciones nuevas para los proyectos

--- src/app/modules/home/projects-list/projects-list.component.ts
+++ src/app/modules/home/projects-list/projects-list.component.ts
@@ -17,7 +17,7 @@ export class ProjectsListComponent {
       repo: 'https://github.com/Franco-Pertusati/growler-front',
       preview: 'https://growler-front.vercel.app/app/dashboard',
       tecs: ['Angular', 'Node.Js', 'Express.Js', 'MySQL'],
-      description: ''
+      description: `This is the demo version of a system for a bar, where products, tables, and cash are managed. It also stores statistics for each shift. The full version is still used in the bar where I used to work, but as a PWA with local storage.`
     },
     {
       title: 'link shortener',
@@ -25,7 +25,7 @@ export class ProjectsListComponent {
       repo: 'https://github.com/Franco-Pertusati/Linktun',
       preview: 'https://franco-pertusati.github.io/Linktun/#/',
       tecs: ['Angular', 'Firebase', 'Node.Js', 'Express.Js'],
-      description: ''
+      description: `A page with user registration using Firebase and an API developed in Node.js to generate shortened links. It's not necessary to create an account to try it out, but links generated without registration are temporary.`
     },
     // {
     //   title: 'UI library',
