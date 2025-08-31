d336e792b9b36a77025eb6c52c8a7ccce356ab80
Franco Pertusati <pertusatifranco836@gmail.com>
2025-08-31 18:45:43 -0300

    Header

--- src/app/modules/landing/landing-page.component.ts
+++ src/app/modules/landing/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { DialogService } from '../../core/services/dialog.service';
+import { ContactComponent } from "./sections/contact/contact.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
